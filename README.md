90cdf4593428e5a15828b0f67c19f8e5abf7c6e5
maxime.care <maxime.care@teads.tv>
2024-06-18 15:44:29 +0200

    Reproduction bug for wdio

---
+++ README.md
@@ -0,0 +1,18 @@
+# Teads web formats
+
+Using node v18.14.0
+
+## Setup
+
+```sh
+npm i 
+```
+
+```sh
+npm start 
+```
+
+And in another terminal
+```sh
+npm run wdio
+```
---
+++ test/specs/exemple.js
@@ -0,0 +1,28 @@
+async function waitForElement(selector) {
+  let element;
+  await browser.waitUntil(
+    async () => {
+      const elementsFound = await $$(selector);
+      element = elementsFound[0];
+      return !!element;
+    },
+    {
+      timeoutMsg: `Element: ${selector} not does not exist after ${browser.options.waitforTimeout}ms`,
+    },
+  );
+  return element;
+}
+
+describe("My Login application", () => {
+  it("should login with valid credentials", async () => {
+    await browser.url("http://localhost:3000/src/ad-served.html");
+    const fixedIframeElement = await waitForElement(
+      "#google_ads_iframe_\\/22474532520\\/velo101\\.com\\/article_2",
+    );
+    await browser.switchToFrame(fixedIframeElement);
+    const iframe = await waitForElement(".div-bad-scroll");
+
+    iframe.scrollIntoView({ block: "center" });
+    await browser.pause(40000);
+  });
+});
